95f56e17d66121ab8da0839b1e703798d87e535b
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-14 22:45:56 -0400

    Importando CSSTransitionsGroup y envolviendo mi imagen en su tag

--- animations-platzi/src/App.js
+++ animations-platzi/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CSSTransitionsGroup from 'react-addons-css-transition-group';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <CSSTransitionsGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </CSSTransitionsGroup>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
