a0ff4949cdfdb089e25276b03a717e330af0ceb6
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-15 14:01:10 -0400

    Mapeando data y agregando keys

--- invie/src/components/Guitarras.js
+++ invie/src/components/Guitarras.js
@@ -4,34 +4,57 @@ import classic from '../media/invie-classic.png';
 
 class Guitarras extends Component {
   render() {
+    const guitarras = [
+      {
+        image: acustica,
+        alt: 'Guitarra Invie Acustica',
+        name: 'Invie Acustica',
+        features: [
+          'Estilo vintage',
+          'Madera pura',
+          'Incluye estuche invisible de aluminio',
+        ],
+      },
+    ]
     return(
       <section id="guitarras" className="guitarras contenedor">
         <h2>Nuestra guitarras</h2>
-        <article className="guitarra">
-          <img className="derecha" src={acustica}  alt="Guitarra Invie Acustica" width="350"/>
-          <div className="contenedor-guitarra-a">
-            <h3 className="title-b">Invie Acustica</h3>
-            <ol>
-              <li>Estilo vintage</li>
-              <li>Madera pura</li>
-              <li>Incluye estuche invisible de aluminio</li>
-            </ol>
-          </div>
-        </article>
-        <article className="guitarra b">
-          <img className="izquierda" src={classic} alt="Guitarra Invie Classic" width="350"/>
-          <div className="contenedor-guitarra-b">
-            <h3 className="title-b">Invie Classic</h3>
-            <ol>
-              <li>Estilo vintage</li>
-              <li>Liviana</li>
-              <li>Inicia tu camino como Rockstar</li>
-            </ol>
-          </div>
-        </article>
+        {
+          guitarras.map( (guitarra, index) => {
+            return(
+              <article className="guitarra" key={index}>
+                <img className="derecha" src={guitarra.image}  alt={guitarra.alt} width="350"/>
+                <div className="contenedor-guitarra-a">
+                  <h3 className="title-b">{guitarra.name}</h3>
+                  <ol>
+                    {
+                      guitarra.features.map((feature, index) => {
+                        return(
+                          <li key={index}>{feature}</li>
+                        )
+                      })
+                    }
+                  </ol>
+                </div>
+              </article>
+            )
+          })
+        }
     </section>
     )
   }
 }
 
 export default Guitarras;
+
+{/* <article className="guitarra b">
+  <img className="izquierda" src={classic} alt="Guitarra Invie Classic" width="350"/>
+  <div className="contenedor-guitarra-b">
+    <h3 className="title-b">Invie Classic</h3>
+    <ol>
+      <li>Estilo vintage</li>
+      <li>Liviana</li>
+      <li>Inicia tu camino como Rockstar</li>
+    </ol>
+  </div>
+</article> */}
